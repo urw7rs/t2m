08a7ba0bc427f8644e8a7a902b2041525aa32a0a
urw7rs <urw.7rs@icloud.com>
2023-10-04 20:25:12 +0900

    Bump version 0.0.1 -> 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="t2m",
-    version="0.0.1",
+    version="0.0.2",
     description="Text To Motion",
     long_description=long_description,
     long_description_content_type="text/markdown",
